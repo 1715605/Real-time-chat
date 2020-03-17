2745b7eb574dfa33ac14cbfb16bcaf789c17f372
1715605 <56392739+1715605@users.noreply.github.com>
2020-03-17 14:05:24 +0000

    modified chat

--- app/src/main/java/com/example/real_time_chat/MainActivity.java
+++ app/src/main/java/com/example/real_time_chat/MainActivity.java
@@ -7,7 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
+
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private Button add_room;
     private EditText room_name;
 
-    private ListView listView;
+
     private ArrayAdapter<String> arrayAdapter;
     private ArrayList<String> list_of_rooms = new ArrayList<>();
     private String name;
@@ -42,64 +42,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        add_room = (Button) findViewById(R.id.btn_add_room);
-        room_name = (EditText) findViewById(R.id.room_name_edittext);
-        listView = (ListView) findViewById(R.id.listView);
-
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-                list_of_rooms);
-        listView.setAdapter(arrayAdapter);
-
         request_user_name();
-        add_room.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Map<String,Object> map = new HashMap<String, Object>();
-                map.put(room_name.getText().toString(),"");
-                root.updateChildren(map);
-
-            }
-        });
-
-        root.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                Set<String> set = new HashSet<String>();
-                Iterator i = dataSnapshot.getChildren().iterator();
-
-                while (i.hasNext()){
-                    set.add(((DataSnapshot)i.next()).getKey());
-                }
-
-                list_of_rooms.clear();
-                list_of_rooms.addAll(set);
-
-                arrayAdapter.notifyDataSetChanged();
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                Intent intent = new Intent(getApplicationContext(),Chat_Room.class);
-                intent.putExtra("room_name",((TextView)view).getText().toString() );
-                intent.putExtra("user_name",name);
-                startActivity(intent);
-            }
-        });
 
     }
 
+    public void change(View view){
+        Intent intent = new Intent(getApplicationContext(),Chat_Room.class);
+        intent.putExtra("room_name",((TextView)view).getText().toString() );
+        intent.putExtra("user_name",name);
+        startActivity(intent);
+
+
+    }
     private void request_user_name() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Enter name:");
